2f57dbcfc5fe44d1d93fec0a7f03e49d185d7a4e
chirix33 <chirixteam@gmail.com>
2024-07-10 09:42:44 +0000

    Update DB Conn

--- admin/includes/database.inc.php
+++ admin/includes/database.inc.php
@@ -6,7 +6,7 @@
 
 	$servername = 'sql210.byethost33.com';
 	$username = 'b33_36872062';
-	$password = 'ashrafhacker';
+	$password = '';
 	$db_name = 'b33_36872062_evoting';
 
 	$conn = mysqli_connect($servername, $username, $password, $db_name);
